08215edabd61d41203e4d21671c07bb960262040
Miroslavv99 <mbochko99@gmail.com>
2025-06-17 11:04:05 +0300

    docs: added comments with explanations of logic

--- KnightsTravails.js
+++ KnightsTravails.js
@@ -18,6 +18,7 @@ class KnightsTravails {
     const [x, y] = position;
     const validPositions = [];
 
+    // Iterate over the array of possible moves and create new moves
     knightMoves.forEach((move) => {
       const dx = move[0];
       const dy = move[1];
@@ -25,6 +26,7 @@ class KnightsTravails {
       const newX = x + dx;
       const newY = y + dy;
 
+      // If the moves do not go beyond the board, then we add to the result
       if (newX >= 0 && newX < this.board && newY >= 0 && newY < this.board) {
         validPositions.push([newX, newY]);
       }
@@ -33,14 +35,17 @@ class KnightsTravails {
   }
 
   knightMoves(start, end) {
+    // Create a queue and a collection of visited positions
     const queue = [[start]];
     const visited = new Set();
 
     while (queue.length > 0) {
+      // Take the positions from queue
       const positions = queue.shift();
       const position = positions[positions.length - 1];
       let positionKey = position.join(",");
 
+      // If position is finish position, return path
       if (positionKey === end.join(",")) {
         console.log(positions);
         return positions;
@@ -48,6 +53,7 @@ class KnightsTravails {
 
       const moves = this.getPossibleMoves(position);
 
+      // We take the next possible move and add it to the queue along with the previous moves
       moves.forEach((move) => {
         if (!visited.has(move.join(","))) {
           queue.push([...positions, move]);
